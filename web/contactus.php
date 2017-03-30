4051231a1efcfe6eced07881a84232f3e7e61f5b
root <root@ip-172-31-47-128.us-west-2.compute.internal>
2017-03-30 22:39:13 +0000

    added video page and other organization

--- web/contactus.php
+++ web/contactus.php
@@ -4,11 +4,14 @@ include($_SERVER['DOCUMENT_ROOT'].'/includes/menu1.php');
 ?>
 
 <div class="content">
+<div class="content-sub1">
+<div class="content-sub2">
 
-<div>
-  <p style="text-align: center;">CONTACT US</p>
-</div> 
+  <p>Contact us page...</p>
+
 <div style="clear:both;"></div>
+</div>
+</div>
 </div>
 
 <?php
--- web/videos/index.php
+++ web/videos/index.php
@@ -8,7 +8,14 @@ include($_SERVER['DOCUMENT_ROOT'].'/includes/menu1.php');
 <div class="content-sub1">
 <div class="content-sub2">
 
-<p>Home page...</p>
+<div>
+<video controls="controls" style="width=:720px; height:400px;" preload="auto" >
+<source src="4dqc-whiteboard-video.mp4" type="video/mp4" />
+<source src="4dqc-whiteboard-video.webm" type="video/webm" />
+Your browser does not support the video tag.
+</video>
+</div>
+
 
 </div>
 </div>
