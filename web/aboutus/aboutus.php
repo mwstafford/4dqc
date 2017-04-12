a2fa24d7f4b408a1a9abf4f43a9506e3e6ac9b99
mwstafford <mstafford@connextron.com>
2017-04-12 09:22:45 -0500

    Update Kevin's bio

--- web/aboutus/aboutus.php
+++ web/aboutus/aboutus.php
@@ -14,11 +14,14 @@ include($_SERVER['DOCUMENT_ROOT'].'/includes/menu1.php');
 <p>The 4DQC team members are seasoned professionals with decades of experience in their
 respective specialties. 
 </p>
+
 <h2>Frederick Strathmann, Ph.D.<br><span style="font-style:italic; font-size:.6em;">Founder and Chief Technical Officer</span></h2>
+
 <p>Dr. Strathmann is also Vice President of Quality Assurance at NMS Laboratories. His major areas of focus have included small molecules such as drugs/metabolites, peptide
 analyses and various endocrinology-related tests using multiplexed liquid chromatography, tandem mass spectrometry and ion mobility technology. 
 Dr. Strathmann is board certified in Clinical Chemistry and Toxicological Chemistry from the American Board of Clinical
 Chemistry and is CTO and founder of 4DQC. He leads the design, function and architecture of the Company.
+</p>
 
 <h2>Scott McClellan<br><span style="font-style:italic; font-size:.6em;">Chief Executive Officer</span></h2>
 <p>Mr. McClellan has more than 25 years of engineering experience.  He holds degrees in bioengineering from UCSD and University of
@@ -26,24 +29,22 @@ Utah and has several successful startup companies, successful IT projects and Bi
 4DQC is to oversee the direction and future opportunities for the Company.
 </p>
 
-
 <h2>Kevin Freeman</h2>
-<p>Mr. Freeman is an extraordinarily talented developer with numerous Data and IT projects to his credit. Mr. Freeman is a graduate of Duke University 
-with 20+ years of software development and database engineering experience. His projects include databases and rapid prototype systems 
-for world-wide human resource management (SRA), multi-billion dollar banking applications, accident and an injury correlation database 
-for NASCAR and FIA. Mr. Freeman is also an accomplished project lead and animator for Sony pictures with no fewer than 17
-major animated feature length films.
+<p>
+Mr. Freeman is an extraordinarily talented developer with numerous Data and IT projects to his credit. Mr. Freeman is a graduate of Duke University with 20+ years of software development and database engineering experience. His projects include databases and rapid prototype systems for world-wide human resource management (SRA), multi-billion dollar banking applications, and accident and injury correlation databases for NASCAR and FIA. Mr. Freeman has also worked as a lead animator with more than a dozen major animated feature length films and gaming projects to his credit.
+</p>
 
 <h2>Erica Cuttitta</h2>
 <p> Ms. Cuttitta has more than 17 years experience in healthcare informatics in a multitude of roles and projects. She has managed projects 
 for corporate software giants and small independent firms as well as large hospitals with healthcare IT database sets and large
-implementation teams. Ms. Cuttitta oversees the project and product development at 4DQC.</p>
+implementation teams. Ms. Cuttitta oversees the project and product development at 4DQC.
+</p>
 
 <h2>Mark Stafford</h2>
 <p>Mr. Stafford has over 20 years experience as a technology solutions architect and has held executive and director positions at several companies.
 He has experience with cloud based systems, big data, micro services, and containerized systems featuring auto-healing and auto-scaling.
 His function for 4DQC is to provide IT support, server maintenance and scalability as well as LIS/LIMS integration when needed.  
-
+</p>
 <p>-------</p>
 	
 <p>4DQC also has a strong group of technical advisors including 
