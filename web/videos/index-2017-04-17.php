d8050875e9ff0a30edbd7f22ea5666897567a3e5
Mark Stafford <mstafford@connextron.com>
2017-04-17 21:16:46 +0000

    new video lightbox

---
+++ web/videos/index-2017-04-17.php
@@ -0,0 +1,31 @@
+<?php
+$title='Videos';
+include($_SERVER['DOCUMENT_ROOT'].'/includes/config1.php');
+include($_SERVER['DOCUMENT_ROOT'].'/includes/header1.php');
+include($_SERVER['DOCUMENT_ROOT'].'/includes/menu1.php');
+?>
+
+<div class="content">
+<div class="content-sub1">
+<div class="content-sub2">
+
+<div>
+<video controls="controls" style="width=:720px; height:400px;">
+<source src="4dqc-whiteboard-video.webm" type="video/webm" />
+<source src="4dqc-whiteboard-video.mp4" type="video/mp4" />
+Your browser does not support the video tag.
+</video>
+</div>
+
+
+<div style="padding-top:100px;">
+<a href="4dqc-whiteboard-video.webm" class="html5lightbox" data-width="480" data-height="320" title="4DQC Whiteboard Video">Video</a>
+</div>
+
+</div>
+</div>
+</div>
+
+<?php
+include($_SERVER['DOCUMENT_ROOT'].'/includes/footer1.php');
+?>
--- web/videos/index.php
+++ web/videos/index.php
@@ -9,8 +9,12 @@ include($_SERVER['DOCUMENT_ROOT'].'/includes/menu1.php');
 <div class="content-sub1">
 <div class="content-sub2">
 
+<p>Click to play: <a href="javascript:void(0)" onclick="document.getElementById('light').style.display='block';document.getElementById('fade').style.display='block';document.getElementById('video1').play();">4DQC Whitebaord Video</a></p>
+
+<div id="light" class="white_content">Click to close: <a href="javascript:void(0)" onclick="document.getElementById('video1').pause();document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none';">Close</a>
+
 <div>
-<video controls="controls" style="width=:720px; height:400px;">
+<video id="video1" controls="controls" style="width=:720px; height:400px;">
 <source src="4dqc-whiteboard-video.webm" type="video/webm" />
 <source src="4dqc-whiteboard-video.mp4" type="video/mp4" />
 Your browser does not support the video tag.
@@ -18,7 +22,11 @@ Your browser does not support the video tag.
 </div>
 
 
-<a href="images/4dqc-whiteboard-video.webm" class="html5lightbox" data-width="480" data-height="320" title="4DQC Whiteboard Video">Video</a>
+</div>
+
+<div id="fade" class="black_overlay"></div>
+
+
 
 </div>
 </div>
